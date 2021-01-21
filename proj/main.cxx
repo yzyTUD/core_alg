14668d1081dcc5e1440cd236a826ac49de30c6b5
yzy <yzhongyuanfff@gmail.com>
2021-01-21 20:09:17 +0100

    first upload

---
+++ proj/main.cxx
@@ -0,0 +1,8 @@
+
+#include "zol1037.h"
+#include "tsp.h"
+
+int main() {
+	zol1037();
+	//tsp();
+}
--- refs/zrlib_cpp_headers_pub/listdirnaems.py
+++
@@ -1,5 +0,0 @@
-import os
-root='.'
-dirlist = [ item for item in os.listdir(root) ]
-for item in dirlist:
-	print(item)
